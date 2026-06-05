953b9490c8bc3be2a4cc65a034b43c2f7cf337b9
Abhinay Bakkera <bakkeraabhinay@gmail.com>
2026-06-05 15:47:48 +0530

    fix live browser loading in project showcase

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const cspHeader = `
   img-src 'self' blob: data: https://firebasestorage.googleapis.com https://www.gstatic.com;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://www.googleapis.com;
-  frame-src 'self' https://orvantia.vercel.app;
+  frame-src 'self' https://orvantia.vercel.app https://continuumos.vercel.app https://www.enteraflux.tech https://clinicalagent.vercel.app;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
